79b48d279eda566d0faa7f809b25b1222462e42b
Hung Nguyen <thaihung1209@yahoo.com.vn>
2016-06-11 00:56:11 +0700

    make symbolic link need to change icon in taskbar of pidgin

---
+++ utility/pidgin-icon.sh
@@ -0,0 +1,20 @@
+ln -sf $HOME/.icons/Mint-X/status/22/user-available-panel.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-available.svg
+ln -sf $HOME/.icons/Mint-X/status/22/user-idle-panel.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-away.svg
+ln -sf $HOME/.icons/Mint-X/status/22/user-busy-panel.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-busy.svg
+ln -sf $HOME/.icons/Mint-X/status/22/user-invisible-panel.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-invisible.svg
+ln -sf $HOME/.icons/Mint-X/status/22/user-offline-panel.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-offline.svg
+ln -sf $HOME/.icons/Mint-X/status/22/tray-new-im.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-email.svg
+ln -sf $HOME/.icons/Mint-X/status/22/user-indeterminate-panel.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-connect.svg
+ln -sf $HOME/.icons/Mint-X/status/22/tray-new-im.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-pending.svg
+ln -sf $HOME/.icons/Mint-X/status/22/tray-new-im.svg $HOME/.icons/Mint-X/status/22/pidgin-tray-xa.svg
+
+ln -sf $HOME/.icons/Mint-X/status/24/user-available-panel.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-available.svg
+ln -sf $HOME/.icons/Mint-X/status/24/user-idle-panel.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-away.svg
+ln -sf $HOME/.icons/Mint-X/status/24/user-busy-panel.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-busy.svg
+ln -sf $HOME/.icons/Mint-X/status/24/user-invisible-panel.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-invisible.svg
+ln -sf $HOME/.icons/Mint-X/status/24/user-offline-panel.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-offline.svg
+ln -sf $HOME/.icons/Mint-X/status/24/tray-new-im.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-email.svg
+ln -sf $HOME/.icons/Mint-X/status/24/user-indeterminate-panel.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-connect.svg
+ln -sf $HOME/.icons/Mint-X/status/24/tray-new-im.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-pending.svg
+ln -sf $HOME/.icons/Mint-X/status/24/tray-new-im.svg $HOME/.icons/Mint-X/status/24/pidgin-tray-xa.svg
+
