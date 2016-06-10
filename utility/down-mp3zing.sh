1c1be8ded61215d556c155169b836d5be1bc3899
Hung Nguyen <thaihung1209@yahoo.com.vn>
2016-06-11 00:24:29 +0700

    correct symbolic link

--- utility/down-mp3zing.sh
+++ utility/down-mp3zing.sh
@@ -1,1 +1,19 @@
-/home/nthung/Dropbox/Script/down-mp3zing.sh
+#! /bin/sh
+
+INPUT=$1
+OUTPUT="output"
+LINK="http://mp3.zing.vn/download/vip/song"
+
+cd $INPUT
+
+for SONG in `ls *.html`
+do
+  FULLLINK=$LINK/$SONG
+  echo $FULLLINK >> $OUTPUT
+  rm -f $SONG
+done
+
+echo "`cat $OUTPUT` files"
+firefox $OUTPUT
+rm -f $OUTPUT
+
--- utility/down-nhaccuatui.sh
+++ utility/down-nhaccuatui.sh
@@ -1,1 +1,21 @@
-/home/nthung/Dropbox/Script/down-nhaccuatui.sh
+# We download the link content actually link to playlist
+wget -q $1
+
+# We get actually link to playlist or music file mix with 'ghost' characters
+cat *.html* | grep player.peConfig.xmlURL | sed 's/\<player.peConfig.xmlURL\>//g' | tr -d ' \";' | sed 's/^=//g' | xargs wget -q
+
+# We remove all 'ghost' characters and save the playlist to file
+cat xml* | grep .mp3 | tr -d ' ' | sed 's/CDATA//g' | tr -d '[]' | sed 's/^<!//g' | sed 's/^--//g' | sed 's/>$//g' | sed 's/--$//g' > playlist
+
+# We count number of music file in the list
+echo "`wc -l playlist`s"
+
+# We download the playlist to our disk
+for MUSIC in `cat playlist`
+do
+	wget $MUSIC
+done
+
+# We remove all temp files
+rm -f playlist *.html* xml*
+
