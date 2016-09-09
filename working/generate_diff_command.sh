24a7a18845b0970eb7499dfc83ae2a0a9a482e43
Hung Nguyen <thaihung1209@yahoo.com.vn>
2016-09-10 01:42:55 +0700

    Add generate hg diff command

---
+++ working/generate_diff_command.sh
@@ -0,0 +1,46 @@
+#! /bash/sh
+
+FILE_PARSING=$1
+START_REVISION=$2
+END_RESVISON=$3
+OUTPUT_DIFF=$4
+PREFIX_DELETE=$5
+
+IS_VALUE=true
+
+if [ -z "$FILE_PARSING" ]; then
+	IS_VALUE=false
+fi
+
+if [ -z "$START_REVISION" ]; then
+	IS_VALUE=false
+fi
+
+if [ -z "$END_RESVISON" ]; then
+	IS_VALUE=false
+fi
+
+if [ -z "$OUTPUT_DIFF" ]; then
+	IS_VALUE=false
+fi
+
+if [ $IS_VALUE = false ]; then
+	echo "Usage: $0 Filename StartRevision EndRevision OutputDiff [PrefixDelete]"
+	echo "\tFilename: File's name include list files"
+	echo "\tStartRevision: Start revision number"
+	echo "\tOutputDiff: File's name that output diff file when run 'hg diff'"
+	echo "\tEndRevision: End revsion number (Start < End)"
+	exit 0
+fi
+
+OUTPUT="$(echo -n "hg diff -r $START_REVISION:$END_RESVISON > $OUTPUT_DIFF.diff -I "
+cat $FILE_PARSING | sed '/^$/d' | sed 's/\\/\//g' | sed ':a;N;$!ba;s/\n/ -I /g')"
+
+
+if [ ! -z "$PREFIX_DELETE" ]; then
+	PREFIX_DELETE=$(echo $PREFIX_DELETE | sed 's/\\/\\\//g')
+	echo $OUTPUT | sed "s/$PREFIX_DELETE//g"
+else
+	echo $OUTPUT
+fi
+
