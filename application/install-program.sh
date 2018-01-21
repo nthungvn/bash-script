c70201233bf6ff29075d7bdd8ea47ad3bf826a11
Hung Nguyen <thaihung1209@yahoo.com.vn>
2018-01-21 15:33:07 +0700

    Update script install for Ubuntu and Mint

--- application/install-program.sh
+++ application/install-program.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
 
-sudo apt-get install stardict
 sudo apt-get install ttf-mscorefonts-installer
-sudo apt-get install skype
-sudo apt-get install gparted
 sudo apt-get install dropbox nemo-dropbox
-sudo apt-get install build-essential
-sudo apt-get install vinagre
 sudo apt-get install git
-sudo apt-get install im-switch
-sudo apt-get install gmail-notify
-sudo apt-get install inkscape python-uniconvertor
-sudo apt-get install openshot frei0r-plugins
-sudo apt-get install gimp-plugin-registry
-
+sudo apt-get install plank
+sudo apt-get install sublime-text
+sudo apt-get install kdiff3-qt
+sudo apt-get install nemo-compare
+# sudo apt-get install gparted
+# sudo apt-get install build-essential
+# sudo apt-get install vinagre
+# sudo apt-get install inkscape python-uniconvertor
+# sudo apt-get install openshot frei0r-plugins
+# sudo apt-get install gimp-plugin-registry
