3c53cf016a79aa4462926ae2d8d7a8a129c23374
Hung Nguyen <nthung.vlvn@gmail.com>
2018-04-03 20:30:01 +0700

    Update key for slack

--- application/external-apps.sh
+++ application/external-apps.sh
@@ -2,17 +2,20 @@
 
 ### add repository
 # skype
-wget -q -O - https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+wget -qO - https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 echo "deb [arch=amd64] https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skype-stable.list
+
 # sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
 # chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -qO - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
 # slack
-echo 'deb https://packagecloud.io/slacktechnologies/slack/debian/ jessie main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget -qO - https://packagecloud.io/slacktechnologies/slack/gpgkey | sudo apt-key add -
+echo 'deb https://packagecloud.io/slacktechnologies/slack/debian/ jessie main' | sudo tee /etc/apt/sources.list.d/slack.list
 
 # sublime-text
 echo 'deb https://download.sublimetext.com/ apt/stable/' | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -21,11 +24,11 @@ echo 'deb https://download.sublimetext.com/ apt/stable/' | sudo tee /etc/apt/sou
 echo 'deb http://linux.teamviewer.com/deb preview tv13' | sudo tee /etc/apt/sources.list.d/teamviewer.list
 
 # atom
-wget -q -O - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" | sudo tee /etc/apt/sources.list.d/atom.list
 
 # vscode
-wget -q -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
 # spotify
