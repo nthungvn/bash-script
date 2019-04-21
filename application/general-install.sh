29d3ee86bef1950c8cd8cad52c5f759d6aa9c27d
Hung Nguyen <nthung.vlvn@gmail.com>
2019-04-21 16:41:59 +0700

    Update and add new apps

--- application/general-install.sh
+++ application/general-install.sh
@@ -9,9 +9,11 @@ sudo apt-get install goldendict
 sudo apt-get install python-qt4 # Only need to run Git-cola
 # sudo apt-get install gparted
 # sudo apt-get install build-essential
-# sudo apt-get install vinagre
+# sudo apt-get install remmina
 # sudo apt-get install inkscape python-uniconvertor
 # sudo apt-get install openshot frei0r-plugins
 # sudo apt-get install gimp-plugin-registry
 # sudo apt-get install gnome-sound-recorder
 # sudo apt-get install kazam
+# sudo apt-get install mplayer # Mirror camera
+# sudo apt-get install baobab # Disk Usage Analyser
