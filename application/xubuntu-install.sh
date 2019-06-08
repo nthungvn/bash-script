456f22d3d14e1d057a45a4851a8c1a0153b8ca6e
Hung Nguyen <nthung.vlvn@gmail.com>
2019-06-08 11:27:31 +0700

    Add ibus-unikey for xubuntu 19.04

--- application/xubuntu-install.sh
+++ application/xubuntu-install.sh
@@ -4,3 +4,4 @@ sudo apt-get install xfce4-time-out-plugin rhythmbox vlc fcitx fcitx-unikey
 sudo apt-get install gnome-disk-utility
 sudo add-apt-repository ppa:numix/ppa && sudo apt-get update && sudo apt-get install numix-gtk-theme numix-icon-theme-circle
 
+sudo apt-get install ibus-unikey # Xubuntu 19.04 can be use, it also good like fcitx-unikey
