3329906d0078806ed6158bcefbfef4e8f4353f8a
Hung Nguyen <nthung.vlvn@gmail.com>
2019-06-29 21:48:01 +0700

    Remove snapd

--- application/xubuntu-remove.sh
+++ application/xubuntu-remove.sh
@@ -2,3 +2,7 @@
 
 sudo apt-get remove gnome-sudoku gnome-mines pidgin thunderbird parole
 sudo apt-get remove sgt-puzzles sgt-launcher xfce4-dict xfburn orage
+
+sudo rm -rf /var/cache/snapd/
+sudo apt autoremove --purge snapd gnome-software-plugin-snap
+rm -fr ~/snap
