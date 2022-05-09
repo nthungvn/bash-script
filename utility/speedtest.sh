af74f64a673367503d529196d6b357ea898641f6
Hung Nguyen <nthung.vlvn@gmail.com>
2022-05-09 21:00:34 +0700

    update speedtest command

--- utility/speedtest.sh
+++ utility/speedtest.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 if [ -z "$1" ]; then
     max=1
 else
@@ -8,10 +10,10 @@ echo "Will run $max tests"
 for i in $(seq 1 $max); do
     echo "Test $i"
     if [ -x "$(command -v curl)" ]; then
-        curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+        curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -
     else
         if [ -x "$(command -v wget)" ]; then
-            wget -qO - https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+            wget -qO - https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -
         else
             echo "Couln't found curl or wget command"
         fi
