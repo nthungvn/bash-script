0cf001e49af7b341e6243480bfbf302e6a63195a
Hung Nguyen <nthung.vlvn@gmail.com>
2026-03-22 15:05:34 +0700

    script to make soft link to the internal drive

---
+++ application/macos-softlink.sh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+
+ln -sfF /Volumes/Macintosh\ HD/Users/nthung/Repos/ $HOME/Repos
+ln -sfF /Volumes/Macintosh\ HD/Users/nthung/vscode-ws/ $HOME/vscode-ws
+ln -sfF /Volumes/Macintosh\ HD/Users/nthung/Downloads/ $HOME/Downloads.ln
